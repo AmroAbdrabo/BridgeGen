8477499f339077363eb0d992af243e656c206c65
AmroAbdrabo <amro.abdrabo@gmail.com>
2024-02-04 09:53:39 +0100

    final soln

--- deck.js
+++ deck.js
@@ -1,15 +1,3 @@
-//DesignModeler JScript, version: Ansys DesignModeler 2023 R1 (Nov 23 2022, 17:11:50; 23,2022,327,1) SV4
-//Created via: "Write Script: Sketch(es) of Active Plane"
-// Written to: C:\Users\amroa\AEC\script_satrude5.js
-//         On: 02/03/24, 15:18:43
-//Using:
-//  agb ... pointer to batch interface
-
-
-//Note:
-// You may be able to re-use below JScript function via cut-and-paste;
-// however, you may have to re-name the function identifier.
-//
 
 function planeSketchesOnly (p)
 {
@@ -27,17 +15,18 @@ p.Sk2.Name = "Sketch2";
 //Edges
 with (p.Sk2)
 {
-  p.Ln5 = Line(0.00000000, 0.8*d32, 0.00000000, -0.8*d32);
-  p.Ln6 = Line(0.00000000, -0.8*d32, 0.00000000 + 1, -10);
+  p.Ln5 = Line(0.00000000, -0.8*10.0, 0.00000000, 0.8*10.0);
+  p.Ln6 = Line(0.00000000, 0.8*10.0, 0.00000000 + 1, 10.0);
 
-  p.Ln7 = Line(0.00000000 + 1, -10.00000000, 0.00000000 + 1, 10.00000000); // base of the deck (d3) 10 should be replaced by d3/2
-  p.Ln8 = Line(0.00000000 + 1, 10.00000000, 5.00000000 + 1, 10.00000000); // lower side of the deck (d4). 5 should be replaced by d4
-  p.Ln9 = Line(5.00000000 + 1, 10.00000000, 5.00000000 + 1, 20.00000000);  // side underbelly of deck. 20 should be replaced by d1/2
-  p.Ln10 = Line(5.00000000 + 1, 20.00000000, 10.00000000 + 1, 20.00000000); // d5. 10 should be replaced by 5 + d5 or d4 + d5
-  p.Ln11 = Line(10.00000000 + 1, 20.00000000, 10.00000000 + 1, -20.00000000); // 20 should be replaced by d1/2. 10 should be replaced by d4 + d5
-  p.Ln12 = Line(10.00000000 + 1, -20.00000000, 5.00000000 + 1, -20.00000000); // 20 should be replaced by d1/2 and 10 should be replaced by d4+d5 and 5 by d4 
-  p.Ln13 = Line(5.00000000 + 1, -20.00000000, 5.00000000 + 1, -10.00000000); // second y-coord should be d3/2 and 5 should be replaced by d4
-  p.Ln14 = Line(5.00000000 + 1, -10.00000000, 0.00000000 + 1, -10.00000000); // 
+  //p.Ln7 = Line(0.00000000 + 1, -10.0, 0.00000000 + 1, 10.0); // base of the deck (20.0) 10 should be replaced by 20.0/2
+  p.Ln8 = Line(0.00000000 + 1, 10.0, 5.0 + 1, 10.0); // lower side of the deck (5.0). 5 should be replaced by 5.0
+  p.Ln9 = Line(5.0 + 1, 10.0, 10.0 - 4.0 + 1, 20.0);  // side underbelly of deck. 20 should be replaced by 40.0/2
+  p.Ln10 = Line(10.0 - 4.0 + 1, 20.0, 10.0 + 1, 20.0); // 4.0. 10 should be replaced by 5 + 4.0 or 5.0 + 4.0
+  p.Ln11 = Line(10.0 + 1, 20.0, 10.0 + 1, -20.0); // 20 should be replaced by 40.0/2. 10 should be replaced by 5.0 + 4.0
+  p.Ln12 = Line(10.0 + 1, -20.0, 10.0 - 4.0 + 1, -20.0); // 20 should be replaced by 40.0/2 and 10 should be replaced by 5.0+4.0 and 5 by 5.0 
+  p.Ln13 = Line(10.0 - 4.0 + 1, -20.0, 5.0 + 1, -10.0); // second y-coord should be 20.0/2 and 5 should be replaced by 5.0
+  p.Ln14 = Line(5.0 + 1, -10.0, 0.00000000 + 1, -10.0); // 
+  p.Ln4 = Line(0.00000000 + 1, -10.0, 0.00000000, -0.8*10.0); // 
 }
 
 //Dimensions and/or constraints
@@ -48,10 +37,10 @@ with (p.Plane)
   HorizontalCon(p.Ln10);
   HorizontalCon(p.Ln12);
   HorizontalCon(p.Ln14);
-  VerticalCon(p.Ln7);
-  VerticalCon(p.Ln9);
+  //VerticalCon(p.Ln7);
+  //VerticalCon(p.Ln9);
   VerticalCon(p.Ln11);
-  VerticalCon(p.Ln13);
+  //VerticalCon(p.Ln13);
 }
 
 p.Plane.EvalDimCons(); //Final evaluate of all dimensions and constraints in plane
@@ -61,7 +50,7 @@ return p;
 
 //Call Plane JScript function
 var ps1 = planeSketchesOnly (new Object());
-var ext1 = agb.Extrude(agc.Add, ps1.Sk2, agc.DirNormal, agc.ExtentFixed, 35.0, agc.ExtentFixed, 0.0, agc.No, 0.0, 0.0);
+var ext1 = agb.Extrude(agc.Add, ps1.Sk2, agc.DirNormal, agc.ExtentFixed, 80.0, agc.ExtentFixed, 0.0, agc.No, 0.0, 0.0);
 
 //Finish
 agb.Regen(); //To insure model validity
